f1c52767775a40bfcb4d54cbfe995e3372cf3cfa
Royston <lekroyston@gmail.com>
2021-10-04 13:28:15 +0800

    testing branched commit

--- CodeJavaApp/src/main/java/net/codejava/CodeJavaApp/User.java
+++ CodeJavaApp/src/main/java/net/codejava/CodeJavaApp/User.java
@@ -8,7 +8,7 @@ public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private Long id;   
 
     @Column(nullable = false, unique = true, length = 45)
     private String email;
