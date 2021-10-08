9b1ff8c0757db0081348288e4962d51422108510
Lye Jian Yi <66357886+lyejy@users.noreply.github.com>
2021-10-08 14:57:03 +0800

    Delete CodeJavaApp/src/main/java/net/codejava/CodeJavaApp/Employee directory

--- CodeJavaApp/src/main/java/net/codejava/CodeJavaApp/Employee/Employee.java
+++
@@ -1,8 +0,0 @@
-package net.codejava.CodeJavaApp.Employee;
-
-import javax.persistence.*;
-import net.codejava.CodeJavaApp.Business.*;
-
-public class Employee {
-
-}
--- CodeJavaApp/src/main/java/net/codejava/CodeJavaApp/Employee/EmployeeRepository.java
+++
@@ -1,4 +0,0 @@
-package net.codejava.CodeJavaApp.Employee;
-
-public interface EmployeeRepository {
-}
