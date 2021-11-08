e1ac2d2da4a0daec45449736f268c1d3beaee34e
ststheting <shaunts5111@gmail.com>
2021-11-08 22:11:54 +0800

    fixed service

--- CodeJavaApp/src/main/java/net/codejava/CodeJavaApp/user/UserService.java
+++ CodeJavaApp/src/main/java/net/codejava/CodeJavaApp/user/UserService.java
@@ -65,13 +65,19 @@ public class UserService {
      */
     public User updateUser(Long userId, User newUser) {
         return users.findById(userId).map(user -> {
-            User findUser = users.findByUsername(newUser.getUsername()).orElse(null);
-            if(findUser != null){
-                throw new UsernameExistsException(newUser.getUsername());
+            if(!newUser.getUsername().equals("dummy@gmail.com")){
+                User findUser = users.findByUsername(newUser.getUsername()).orElse(null);
+                if(findUser != null){
+                    throw new UsernameExistsException(newUser.getUsername());
+                }
+                user.setUsername(newUser.getUsername());
             }
-            user.setPassword(encoder.encode(newUser.getPassword()));
-            user.setFirstName(newUser.getFirstName());
-            user.setLastName(newUser.getLastName());
+            else{
+
+                user.setPassword(encoder.encode(newUser.getPassword()));
+                user.setFirstName(newUser.getFirstName());
+                user.setLastName(newUser.getLastName());
+            }
             return users.save(user);
         }).orElseThrow(() -> new UserNotFoundException(userId));
     }
